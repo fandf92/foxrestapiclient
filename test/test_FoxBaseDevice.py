fdad2b0c3fa246809da2772df7c35eaffcd7e897
Tobiasz <t.waclawiak@fif.com.pl>
2021-10-19 11:26:48 +0000

    Updated tests for F&F Fox base device class.

--- test/test_FoxBaseDevice.py
+++ test/test_FoxBaseDevice.py
@@ -8,7 +8,6 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from foxrestapiclient.devices.fox_base_device import FoxBaseDevice, UnsupportedDevice
 from .const import (
     API_KEY,
-    CHANNELS,
     HOST,
     NAME,
     TYPE,
@@ -24,29 +23,39 @@ def async_test(coro):
     return wrapper
 
 class FoxBaseDeviceTest(unittest.TestCase):
-    def test_init_base_device(self):
-        try:
-            FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE)
-        except UnsupportedDevice:
-            self.assertFalse(True)
-        self.assertFalse(False)
+    device = FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE)
 
     def test_equals(self):
-        self.assertEqual(FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE).equals(
-            FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE)), True)
+        self.assertTrue(self.device.equals(
+            FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE)))
+
+    def test_get_channel_name(self):
+        self.assertIsNotNone(self.device.get_channel_name(0))
+        self.assertIsNotNone(self.device.get_channel_name(1))
+        self.assertIsNotNone(self.device.get_channel_name(2))
+        self.assertIsNotNone(self.device.get_channel_name(10))
+        self.assertIsNotNone(self.device.get_channel_name(-1))
 
     def test_get_device_info(self):
-        info = FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE).get_device_info()
-        if info != "":
-            self.assertTrue(True)
-        else:
-            self.assertTrue(False)
+        device_info = self.device.get_device_info()
+        self.assertIsNotNone(device_info["identifiers"])
+        self.assertIsNotNone(device_info["name"])
+        self.assertIsNotNone(device_info["manufacturer"])
+        self.assertIsNotNone(device_info["model"])
+        self.assertIsNotNone(device_info["sw_version"])
 
     @async_test
-    async def test_async_fetch_device_available_data(self):
-        """If device is not available this test will be pass"""
-        dev = FoxBaseDevice(NAME, HOST, API_KEY, UNIQUE_ID, TYPE)
-        await dev.async_fetch_device_available_data()
+    async def test_async_fetch_channel_state(self):
+        state = await self.device.async_fetch_channel_state()
+        self.assertIn(state, [True, False])
+
+    @async_test
+    async def test_async_update_channel_state(self):
+        self.assertTrue((await self.device.async_update_channel_state(False)))
+
+    @async_test
+    async def test_async_fetch_device_info(self):
+        self.assertTrue((await self.device.async_fetch_device_info()))
 
 if __name__ == '__main__':
     unittest.main()
